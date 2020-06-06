ab17e30050176ccd2f3bc597d2d61fd7af0a8680
SternHillpocken <nicolas.guerin.pro@laposte.net>
2020-06-06 14:34:20 +0200

    structure page base

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      {/*}<header className="App-header">
+      </header>*/}
+      <div id="resources-div">Resources</div>
+      <div id="tabs-div">Tabs</div>
+      <div id="game-informations-div">Informations</div>
+      <div id="game-text-div">Text</div>
+      <footer>Footer de merde</footer>
     </div>
   );
 }
