36af14d1f9a05fd9412d3a862d213b007f4d1b59
FRosay <felix.rosay@laposte.net>
2020-08-19 17:11:28 +0200

    better name generator

--- src/components/misc-tools/name-generator/NameGenerator.js
+++ src/components/misc-tools/name-generator/NameGenerator.js
@@ -1,36 +1,41 @@
-const lettersAll = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] // 26
-const lettersVoyels = ['A', 'E', 'I', 'O', 'U', 'Y'] // 6
-const pickChanceVoyels = [20, 25, 20, 15, 15, 5] // as %
-const lettersConsonents = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'] // 20
+const lettersAll = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] // 26
+//const pickChanceAll = [10, 2, 3, 4, 14, 1, 2, 1, 8, 1, 2, 6, 3, 6, 5, 5, 2, 6, 6, 6, 5, 1, 0.5, 0.5, 0.5, 0.5] // as %
+const lettersVoyels = ['a', 'e', 'i', 'o', 'u', 'y'] // 6
+//const pickChanceVoyels = [30, 35, 15, 8, 8, 4] // as %
+const lettersConsonents = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'] // 20
 
 function NameGenerator(minLength, maxLength) {
-  
-    let generatedName = ''
+
+    let generatedName = ['', '']
     let rng = Math.floor(Math.random() * 26)
     let nameLength = Math.random() * (maxLength - minLength) + minLength
+    let result
 
     for (let i = 0; i < nameLength; i++) {
-        let lastLetter
 
-        if (i===0) {
-            generatedName += lettersAll[rng]
-            lastLetter = lettersAll[rng]
+        let lastLetter          = generatedName[generatedName.length-1]
+        let secondToLastLetter  = generatedName[generatedName.length-2]
+
+        if (lastLetter === secondToLastLetter) { // Pas plus de 2x la même lettre à la suite
+            while (lettersAll[rng] === lastLetter) {
+                rng = Math.floor(Math.random() * 26)
+            }
+            generatedName.push(lettersAll[rng])
         } else {
-            if (lettersVoyels.includes(lastLetter)) {
+
+            if (lettersVoyels.includes(lastLetter)) { // Mettre une consonne après une voyelle
                 rng = Math.floor(Math.random() * 20)
-                generatedName += lettersConsonents[rng].toLowerCase()
-                lastLetter = lettersConsonents[rng]
+                generatedName.push(lettersConsonents[rng])
             } else {
-                generatedName += lettersAll[rng].toLowerCase()
-                lastLetter = lettersAll[rng]
+                generatedName.push(lettersAll[rng])
             }
         }
-        
-
         rng = Math.floor(Math.random() * 26)
     }
 
-    return generatedName
+    result = generatedName.slice(2)
+
+    return result
 }
 
 export default NameGenerator;
