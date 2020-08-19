7a69d8c3147f38a3278709cf1678aa9afa176679
FRosay <felix.rosay@laposte.net>
2020-08-20 01:25:47 +0200

    added letter probabilities to the name generator

--- src/components/misc-tools/name-generator/NameGenerator.js
+++ src/components/misc-tools/name-generator/NameGenerator.js
@@ -1,8 +1,9 @@
 const lettersAll = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] // 26
-//const pickChanceAll = [10, 2, 3, 4, 14, 1, 2, 1, 8, 1, 2, 6, 3, 6, 5, 5, 2, 6, 6, 6, 5, 1, 0.5, 0.5, 0.5, 0.5] // as %
+const pickChanceAll = [10, 12, 15, 19, 33, 34, 36, 37, 45, 46, 48, 54, 58, 62, 67, 72, 74, 79, 85, 90, 95, 96, 97, 98, 99, 100] // as %
 const lettersVoyels = ['a', 'e', 'i', 'o', 'u', 'y'] // 6
-//const pickChanceVoyels = [30, 35, 15, 8, 8, 4] // as %
+const pickChanceVoyels = [30, 65, 80, 88, 96, 100] // as %
 const lettersConsonents = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'] // 20
+const pickChanceConsonents = [5, 9, 16, 18, 21, 23, 25, 29, 40, 43, 51, 61, 66, 72, 83, 92, 94, 96, 98, 100] // as %
 
 function NameGenerator(minLength, maxLength) {
 
@@ -11,26 +12,65 @@ function NameGenerator(minLength, maxLength) {
     let nameLength = Math.random() * (maxLength - minLength) + minLength
     let result
 
+    function pickAnyLetter(letterToAvoid) {
+        let newLetter = ''
+        let iteration = 0
+
+        while ((rng >= pickChanceAll[iteration]) && (lettersAll[iteration] !== letterToAvoid)) {
+            iteration++
+        }
+
+        newLetter = lettersAll[iteration]
+        generatedName.push(newLetter)
+    }
+
+    function pickConsonent() {
+        let consonent = ''
+        let iteration = 0
+
+        while (rng >= pickChanceConsonents[iteration]) {
+            iteration++
+        }
+
+        consonent = lettersConsonents[iteration]
+        generatedName.push(consonent)
+    }
+
+    function pickVoyel() {
+        let voyel = ''
+        let iteration = 0
+
+        while (rng > pickChanceVoyels[iteration]) {
+            iteration++
+        }
+
+        voyel = lettersVoyels[iteration]
+        generatedName.push(voyel)
+    }
+
     for (let i = 0; i < nameLength; i++) {
 
         let lastLetter          = generatedName[generatedName.length-1]
         let secondToLastLetter  = generatedName[generatedName.length-2]
+        let thirdToLastLetter   = generatedName[generatedName.length-3]
 
-        if (lastLetter === secondToLastLetter) { // Pas plus de 2x la même lettre à la suite
-            while (lettersAll[rng] === lastLetter) {
-                rng = Math.floor(Math.random() * 26)
-            }
-            generatedName.push(lettersAll[rng])
+        rng = Math.floor(Math.random() * (100 - 1) + 1)
+
+        if (lastLetter === secondToLastLetter) { 
+            // Après une lettre doublée, on force le changement
+            pickAnyLetter(lastLetter)
+
+        } else if (lettersVoyels.includes(lastLetter)) { 
+            // Après une voyelle, on met une consonne
+            pickConsonent()
+
+        } else if (lettersConsonents.includes(lastLetter) && lettersConsonents.includes(secondToLastLetter) && lettersConsonents.includes(thirdToLastLetter)) {
+            // Après 3 consonnes, on met une voyelle
+            pickVoyel()
+
         } else {
-
-            if (lettersVoyels.includes(lastLetter)) { // Mettre une consonne après une voyelle
-                rng = Math.floor(Math.random() * 20)
-                generatedName.push(lettersConsonents[rng])
-            } else {
-                generatedName.push(lettersAll[rng])
-            }
+            pickAnyLetter('')
         }
-        rng = Math.floor(Math.random() * 26)
     }
 
     result = generatedName.slice(2)
