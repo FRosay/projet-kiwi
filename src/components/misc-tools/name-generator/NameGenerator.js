716c3382517c7f74a6a9fa5bcb3943aee69eb085
FRosay <felix.rosay@laposte.net>
2020-08-21 14:01:06 +0200

    rng bugfixing

--- src/components/misc-tools/name-generator/NameGenerator.js
+++ src/components/misc-tools/name-generator/NameGenerator.js
@@ -7,7 +7,7 @@ const lettersConsonents         = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
 function NameGenerator(minLength, maxLength, requiredTitle, forcedLetters = [], forcedLettersPosition = [-1,-1]) {
 
     let result
-    let generatedName                   = ['', '']
+    let generatedName                   = []
     let nameLength                      = Math.floor(Math.random() * (maxLength - minLength) + minLength)
     let firstForcedLettersPositioning
     let secondForcedLettersPositioning
@@ -29,47 +29,46 @@ function NameGenerator(minLength, maxLength, requiredTitle, forcedLetters = [], 
     }
 
     if (parseInt(forcedLettersPosition[0], 10) === -1) {
-        firstForcedLettersPositioning = Math.floor(Math.random() * Math.floor(nameLength + 1))
+        firstForcedLettersPositioning = Math.floor(Math.random() * Math.floor(nameLength))
     } else {
-        firstForcedLettersPositioning = parseInt(forcedLettersPosition[0], 10)
+        firstForcedLettersPositioning = (parseInt(forcedLettersPosition[0], 10)-1)
     }
     if (parseInt(forcedLettersPosition[1], 10) === -1) {
-        secondForcedLettersPositioning = Math.floor(Math.random() * Math.floor(nameLength + 1))
+        secondForcedLettersPositioning = Math.floor(Math.random() * Math.floor(nameLength))
     } else {
-        secondForcedLettersPositioning = parseInt(forcedLettersPosition[1], 10)
+        secondForcedLettersPositioning = (parseInt(forcedLettersPosition[1], 10)-1)
     }
-
+    
     for (let i = 0; i < nameLength; i++) {
 
         let lastLetter          = generatedName[generatedName.length-1]
         let secondToLastLetter  = generatedName[generatedName.length-2]
         let thirdToLastLetter   = generatedName[generatedName.length-3]
-        
-        // On met -1 car le choix de la position commence à 1, mais l'array à 0
-        if (i === firstForcedLettersPositioning) {
+
+        if (forcedLetters[0][0] !== '' && i === firstForcedLettersPositioning) {
             forcedLetters[0].forEach(letter => generatedName.push(letter))
             
-        } else if (i === secondForcedLettersPositioning) {
+        } else if (forcedLetters[1][0] !== '' && i === secondForcedLettersPositioning) {
             forcedLetters[1].forEach(letter => generatedName.push(letter))
             
         } else if (lastLetter === secondToLastLetter) { 
             // Après une lettre doublée, on force le changement
-            generatedName.push(AddLetter('any'), lastLetter)
-
+            generatedName.push(AddLetter('any', lastLetter))
+            
         } else if (lettersVoyels.includes(lastLetter)) { 
             // Après une voyelle, on met une consonne
             generatedName.push(AddLetter('consonent'))
-
+            
         } else if (lettersConsonents.includes(lastLetter) && lettersConsonents.includes(secondToLastLetter) && lettersConsonents.includes(thirdToLastLetter)) {
             // Après 3 consonnes, on met une voyelle
             generatedName.push(AddLetter('voyel'))
-
+            
         } else {
             generatedName.push(AddLetter('any'))
+            
         }
     }
-
-    generatedName = generatedName.slice(2)
+    
     generatedName[0] = generatedName[0].toUpperCase()
     
     requiredTitle.includes(true) ? result = generatedName.toString().replace(/,/g, '') + ' ' + addTitle() : result = generatedName.toString().replace(/,/g, '')
